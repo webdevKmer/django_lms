283007d56c32f7bfa4a8230c675e7d968b964623
Web Dev Kmer <webdev.kmer@gmail.com>
2022-12-27 05:09:25 +0100

    creating first views and user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class user_profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=150, blank=True)
+    profile_pic = models.ImageField(upload_to='',blank=True)
+
+    teacher = "teacher"
+    student = "student"
+    parent = "parent"
+    user_types = [
+        (teacher, 'teacher'),
+        (student, 'student'),
+        (parent, 'parent'),
+    ]
+
+    user_type = models.CharField(max_length=10, choices=user_types, default=student)
